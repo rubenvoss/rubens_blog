df4714c45bb8694c4ddc0e8b50dda3854864aa01
Ruben Voß <bekertshop@gmail.com>
2024-03-29 18:42:17 +0100

    add post/ view

--- rubens_blog/posts/views.py
+++ rubens_blog/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+import markdown
 
-# Create your views here.
+def post_view(request):
+    post = Post.objects.first()
+    md = markdown.Markdown(extensions=["fenced_code"])
+    post.content = md.convert(post.content)
+    context = {"post": post}
+    return render(
+        request,
+        "posts/post.html",
+        context=context
+    )
--- rubens_blog/rubens_blog/urls.py
+++ rubens_blog/rubens_blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import post_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("post/", post_view, name="post"),
+
 ]
