f6e1a22c0de6990f26157307c14f5376b92c66bd
Ruben Voß <bekertshop@gmail.com>
2024-03-30 10:28:05 +0100

    convert_markdown template filter

---
+++ rubens_blog/posts/templatetags/post_conversion.py
@@ -0,0 +1,12 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+import markdown
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def convert_markdown(content):
+    md = markdown.Markdown(extensions=["fenced_code"])
+    return mark_safe(md.convert(content))
--- rubens_blog/posts/views.py
+++ rubens_blog/posts/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from .models import Post
-import markdown
 
 def post_view(request):
     post = Post.objects.first()
-    md = markdown.Markdown(extensions=["fenced_code"])
-    post.content = md.convert(post.content)
     context = {"post": post}
     return render(
         request,
