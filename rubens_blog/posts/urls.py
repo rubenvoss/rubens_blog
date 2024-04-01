14d7f58ea6e4d3b476af532e5be193f1b39431b0
Ruben Voß <bekertshop@gmail.com>
2024-04-01 19:05:56 +0200

    namechange

--- rubens_blog/posts/urls.py
+++ rubens_blog/posts/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", PostListView.as_view(), name="posts"),
     path("add/", PostCreateView.as_view(), name="post_create"),
     path("<int:pk>", PostDetailView.as_view(), name="post_detail"),
-    path("<int:pk>/edit/", PostUpdateView.as_view(template_name="posts/post_update.html"), name="post_update"),
+    path("<int:pk>/update/", PostUpdateView.as_view(template_name="posts/post_update.html"), name="post_update"),
 ]
