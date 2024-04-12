dc7f5e48c6c949bc31143db6911a18991d8a75b7
Ruben Voß <bekertshop@gmail.com>
2024-04-12 20:11:31 +0200

    url redirect

--- rubens_blog/rubens_blog/urls.py
+++ rubens_blog/rubens_blog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("posts/", include("posts.urls")),
+    path("", RedirectView.as_view(url="posts/", permanent=False), name="index")
 ]
