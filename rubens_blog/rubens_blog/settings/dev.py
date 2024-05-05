d559af1c842ee449e3933b923eff8206a38a264a
Ruben Voß <bekertshop@gmail.com>
2024-05-05 19:01:28 +0200

    wagtail init

---
+++ rubens_blog/rubens_blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-iuw2$b1l*_rm4*o16407a^0^s%u)06)u3%jurna_6n#rbgeor0"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
