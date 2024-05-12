284abe5b9c9d508aadc15c8333cb6489a127c52f
Ruben Voß <bekertshop@gmail.com>
2024-05-12 15:00:32 +0200

    update

--- production/update.sh
+++ production/update.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+export ENV_NAME=production
 
 /usr/bin/git pull
 
 # collectstatic?
 /srv/www/rubens_blog/venv/bin/python /srv/www/rubens_blog/rubens_blog/manage.py collectstatic --noinput
 
+# install new reqs
+/srv/www/rubens_blog/venv/bin/pip install -r /srv/www/rubens_blog/requirements.txt
+
+# migrate db
+/srv/www/rubens_blog/venv/bin/python /srv/www/rubens_blog/rubens_blog/manage.py migrate
+
 # restart gunicorn
 systemctl reload gunicorn
 
-# postgresql  manage.py migrate?
