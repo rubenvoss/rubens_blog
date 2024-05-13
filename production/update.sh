6851dd165a775b60fc3a428a32994218d417f15f
Ruben Voß <bekertshop@gmail.com>
2024-05-13 12:32:03 +0200

    test

--- production/update.sh
+++ production/update.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-echo "export ENV_NAME=production"
+echo "[EXECUTING COMMAND] export ENV_NAME=production"
 export ENV_NAME=production
 
-echo "/usr/bin/git pull"
+echo "[EXECUTING COMMAND] /usr/bin/git pull"
 /usr/bin/git pull
 
 # collect static files
-echo "/srv/www/rubens_blog/venv/bin/python /srv/www/rubens_blog/rubens_blog/manage.py collectstatic --noinput"
+echo "[EXECUTING COMMAND] /srv/www/rubens_blog/venv/bin/python /srv/www/rubens_blog/rubens_blog/manage.py collectstatic --noinput"
 /srv/www/rubens_blog/venv/bin/python /srv/www/rubens_blog/rubens_blog/manage.py collectstatic --noinput
 
 # install new reqs
-echo "/srv/www/rubens_blog/venv/bin/pip install -r /srv/www/rubens_blog/requirements.txt | grep -v 'Requirement already satisfied:'"
+echo "[EXECUTING COMMAND] /srv/www/rubens_blog/venv/bin/pip install -r /srv/www/rubens_blog/requirements.txt | grep -v 'Requirement already satisfied:'"
 /srv/www/rubens_blog/venv/bin/pip install -r /srv/www/rubens_blog/requirements.txt | grep -v 'Requirement already satisfied:'
 
 # migrate db
-echo "/srv/www/rubens_blog/venv/bin/python /srv/www/rubens_blog/rubens_blog/manage.py migrate"
+echo "[EXECUTING COMMAND] /srv/www/rubens_blog/venv/bin/python /srv/www/rubens_blog/rubens_blog/manage.py migrate"
 /srv/www/rubens_blog/venv/bin/python /srv/www/rubens_blog/rubens_blog/manage.py migrate
 
 # restart gunicorn
-echo "systemctl reload gunicorn"
+echo "[EXECUTING COMMAND] systemctl reload gunicorn"
 systemctl reload gunicorn
