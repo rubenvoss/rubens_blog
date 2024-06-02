eef4745d9ae8d7494cf3e368e58ef1a998bf42b3
Ruben Voß <bekertshop@gmail.com>
2024-06-02 22:11:05 +0200

    remove certfiles

--- rubens_blog/gunicorn_config.py
+++ rubens_blog/gunicorn_config.py
@@ -5,5 +5,5 @@ workers = 4  # Adjust based on your server's resources
 worker_connections = 1000
 threads = 4
 
-certfile = "/etc/letsencrypt/live/rubenvoss.de/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/rubenvoss.de/privkey.pem"
+# certfile = "/etc/letsencrypt/live/rubenvoss.de/fullchain.pem"
+# keyfile = "/etc/letsencrypt/live/rubenvoss.de/privkey.pem"
