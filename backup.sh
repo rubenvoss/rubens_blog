95f7bb09dc2c06ddec9c9fd7fbccfa7d7ffe883c
Ruben Voß <bekertshop@gmail.com>
2024-06-19 22:11:01 +0200

    vars, folder

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DATE=$(date +%Y%m%d-%H%M%S)
-/usr/bin/docker exec -it postgres pg_dumpall -U ruben | gzip -9c > /root/db-$DATE.sql.gz
-echo "Backup db-$DATE.sql.gz erstellt" >> /root/backup.log
+BACKUP_FILE=/root/backups/db-$DATE.sql.gz
+/usr/bin/docker exec -it postgres pg_dumpall -U ruben | gzip -9c > $BACKUP_FILE
+echo "Backup $BACKUP_FILE erstellt" >> /root/backup.log
